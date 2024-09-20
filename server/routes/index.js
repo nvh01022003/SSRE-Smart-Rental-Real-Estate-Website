4bdd35c525670cefb12652305224344f6a15a03f
trile03x <levantri6839@gmail.com>
2024-09-20 08:21:38 +0700

    project initialization

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const authRouter = require("./auth")
-const initRoutes = (app) => {
-    app.use("/api/v1/auth", authRouter)
-    return app.use("/", (req, res) => {
-        res.send("oke server on!")
-    })
-}
-module.exports = initRoutes
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const connectData = require("./src/config/connect-db")
 const cors = require("cors")
-const initRoutes = require("./routes/index")
+const initRoutes = require("./src/services/routes/index")
 dotenv.config()
+// connect database
+// connectData()
 const app = express()
 app.use(cors({
     origin: process.env.CLIENT_URL,
