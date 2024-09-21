d9e47f7871c394429218c47ac6ec58fbbf81abed
trile03x <levantri6839@gmail.com>
2024-09-22 01:01:00 +0700

    update folder

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express")
 const dotenv = require("dotenv")
 const connectData = require("./src/config/connect-db")
 const cors = require("cors")
-const initRoutes = require("./src/services/routes/index")
+const initRoutes = require("./src/routes/index")
 dotenv.config()
-connect database
-connectData()
+// connect database
+// connectData()
 const app = express()
 app.use(cors({
     origin: process.env.CLIENT_URL,
