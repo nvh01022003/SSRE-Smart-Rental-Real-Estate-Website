a63f4361206c1103772ba75bb0db20fd8660b092
trile03x <levantri6839@gmail.com>
2024-09-26 23:21:49 +0700

    check exits email+phone

--- server/src/middleware/auth-email.js
+++ server/src/middleware/auth-email.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer")
 const validator = require('validator');
 const { where } = require("sequelize");
 const readline = require('readline');
-const { VeriMail, sequelize } = require("../models/index");
+const { VeriMail, User, sequelize } = require("../models/index");
+const { error } = require("console");
+// gui code den email nguoi dung
 const createCodeVery = async (req, res, next) => {
     const { firstName, lastName, numberPhone, email, password } = req.body
     if (!firstName || !lastName || !numberPhone || !email || !password) {
@@ -32,7 +34,7 @@ const createCodeVery = async (req, res, next) => {
             let mailOptions = {
                 from: process.env.EMAIL_USER, // Email người gửi
                 to: email, // Email người nhận
-                subject: 'Mã xác thực của bạn cho SSRE là:',
+                subject: `Chào ${firstName} Mã xác thực của bạn cho SSRE là:`,
                 text: `Mã xác thực của bạn là: ${verificationCode}`,
             }
             try {
@@ -56,10 +58,11 @@ const createCodeVery = async (req, res, next) => {
         }
     }
 }
+// client nhập code mail để so sanh với code trong db
 const verifiedMail = async (req, res, next) => {
     const { email } = req.body
     // tao code fake de test nek
-    const codeEmail = "140420023"
+    const codeEmail = "14042003"// nhap vao
     const resCode = await VeriMail.findOne({ where: { email } });
     if (codeEmail == resCode.code) {
         next();
@@ -68,4 +71,32 @@ const verifiedMail = async (req, res, next) => {
         res.status(200).send("Very email fail!")
     }
 }
-module.exports = { createCodeVery, verifiedMail }
+// check email va sdt da ton tai ch
+const validateEmailPhone = async (req, res, next) => {
+    const { email, numberPhone } = req.body
+    try {
+        const existingEmail = await User.findOne({ where: { email } });
+        const existingPhone = await User.findOne({ where: { phone: numberPhone } });
+        if (existingEmail) {
+            return res.status(200).json({
+                err: 0,
+                msg: 'Email already exists'
+            });
+        }
+        else if (existingPhone) {
+            return res.status(200).json({
+                err: 0,
+                msg: 'Phone number already exists'
+            });
+        }
+        else
+            next()
+    }
+    catch (err) {
+        return res.status(400).json({
+            err: 10,
+            msg: err.message
+        });
+    }
+}
+module.exports = { createCodeVery, verifiedMail, validateEmailPhone }
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,6 +2,6 @@ const express = require("express")
 const authController = require("../controller/auth")
 const middleware = require("../middleware/auth-email")
 const router = express.Router()
-router.post("/register", middleware.createCodeVery, middleware.verifiedMail, authController.resgister)
+router.post("/register", middleware.validateEmailPhone, middleware.createCodeVery, middleware.verifiedMail, authController.resgister)
 router.post("/login", authController.login)
 module.exports = router
