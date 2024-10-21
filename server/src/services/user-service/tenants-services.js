62f6596b47efc33ecd2cf1dd29271e23623d90a3
trile03x <levantri6839@gmail.com>
2024-10-22 00:34:24 +0700

    update 22/10

--- server/src/services/user-service/tenants-services.js
+++ server/src/services/user-service/tenants-services.js
@@ -234,7 +234,7 @@ const showDetailPost = async (postId) => {
             }
         })
         // tim chi tiet cac bang khac : category, address, user, overviews dùng promise.all
-        const [category, address, user, overviews] = await Promise.all([
+        const [category, address, user, overviews, map] = await Promise.all([
             Category.findOne({
                 where: {
                     id: post.category_id
@@ -267,7 +267,8 @@ const showDetailPost = async (postId) => {
             })
 
         ])
-        post.dataValues.map = `<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7668.902703874087!2d${overviews.dataValues.lon}!3d${overviews.dataValues.lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1729530897356!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+        console.log(map);
+        post.dataValues.map = `<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7668.902703874087!2d${map.dataValues.lon}!3d${map.dataValues.lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1729530897356!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
         post.dataValues.category = category;
         post.dataValues.address = address;
         post.dataValues.user = user;
